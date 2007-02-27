0eadcd24e3ed2b741709e14297769fe65a45775e
enriqueplace <enriqueplace@f1ab2e10-6d2a-0410-a616-39d52dd4c448>
2007-02-27 05:05:20 +0000

    Primera versión sistema de ejemplo "Proyecto Base"

---
+++ surforce-proyectobase/validar_formulario.js
@@ -0,0 +1,14 @@
+function validar(){
+      var frm = document.forms["form1"];
+      var msg = new String();
+
+      if(frm.id.value =="" && frm.nombre.value ==""){
+         msg = "Debe ingresar informacion en alguno de los campos";
+      }
+
+      if(msg.length > 0){
+         alert('Atencion: '+msg);
+      }else{
+         frm.submit();
+      }
+}
