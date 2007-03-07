650185bfca8530a97a589d0c7d0024903ef6f361
enriqueplace <enriqueplace@f1ab2e10-6d2a-0410-a616-39d52dd4c448>
2007-03-07 03:27:15 +0000

    - cambia estética (css, templates) - se agrega un campo nuevo "descripción"

--- trunk/surforce-proyectobase/presentacion/PresentacionFachada.class.php
+++ trunk/surforce-proyectobase/presentacion/PresentacionFachada.class.php
@@ -1,11 +1,10 @@
 <?php
-
 require_once 'configuracion.php';
+require_once("TemplateSmarty.class.php");
 
 abstract class PresentacionFachada {
 
 	public function listarUsuarios($usuarios){
-		require_once("TemplateSmarty.class.php");
 		$miSmarty = new TemplateSmarty();
 		$miSmarty->assign("titulo", "Listado de usuarios");
 		$miSmarty->assign("usuarios", $usuarios);
@@ -13,27 +12,22 @@ abstract class PresentacionFachada {
 		$miSmarty->display("sitio_estructura_estandar.tpl.html");
 	}
 	public function mostrarTexto($titulo, $texto){
-		require_once("TemplateSmarty.class.php");
 		$miSmarty = new TemplateSmarty();
-		
 		$miSmarty->assign("titulo", $titulo);
 		$miSmarty->assign("texto", $texto);
 		$miSmarty->assign("sitio_contenido", "contenido_index.tpl.html");
 		$miSmarty->display("sitio_estructura_estandar.tpl.html");
 	}
 	public function consultarUsuario(){
-		require_once("TemplateSmarty.class.php");
 		$miSmarty = new TemplateSmarty();
 		$miSmarty->assign("titulo", "Consultar Usuarios");
 		$miSmarty->assign("sitio_contenido", "contenido_consultar_usuario.tpl.html");
 		$miSmarty->display("sitio_estructura_estandar.tpl.html");
 	}
 	public function mostrarUsuario($unUsuario){
-		require_once("TemplateSmarty.class.php");
 		$miSmarty = new TemplateSmarty();
 		$miSmarty->assign("titulo", " Usuario");
 		$miSmarty->assign("usuario",$unUsuario);
-		
 		$miSmarty->assign("sitio_contenido", "contenido_mostrar_usuario.tpl.html");
 		$miSmarty->display("sitio_estructura_estandar.tpl.html");
 	}
